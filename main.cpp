0f84586d471d8bfb3081ad00cd084b4346853d78
Wojciech Wójcik <wojtek.wroclaw@hotmail.com>
2019-03-20 21:23:24 +0100

    main function, started working on threads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
-    std::cout << "Hello World!" << std::endl;
+    
+    int threadCount;
+
+    if (argc != 2) {
+
+        std::cout << "Brak argumentu z liczba watkow" << std::endl;
+        return 1;
+    }
+    else {
+
+        std::string str = argv[1];
+        threadCount = std::stoi(str);
+    }
+
+    if(threadCount<1 || threadCount > 20){
+        std::cout << "Liczba watkow miedzy 1 a 20" << std::endl;
+        return 2;
+    }
+
+    char c = 's';
+    while(c != 'q')
+    {
+        std::cin >> c;
+    }
+
+    std::cout<< "Koniec Programu!"<< std::endl;
+
     return 0;
 }
